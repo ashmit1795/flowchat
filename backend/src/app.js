46196f41575f76c873259a788f1f829381dc3db5
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-11-30 23:16:49 +0530

    refactor: reorganize middleware imports and app initialization for clarity

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { authRouter } from "./routes/auth.routes.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import cookieParser from "cookie-parser";
 
-export const app = express();
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+	res.send("Hello, World!");
 });
 
 app.use("/api/auth", authRouter);
+
+export { app };
