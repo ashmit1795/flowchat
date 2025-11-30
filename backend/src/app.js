1ad20cec89607c2303bc4dfb9e35010792239da9
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-11-30 23:26:33 +0530

    fix: correct import path for error handler middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authRouter } from "./routes/auth.routes.js";
-import errorHandler from "./middlewares/errorHandler.js";
+import errorHandler from "./middlewares/errorHandler.middleware.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -9,12 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(errorHandler);
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
 app.use("/api/auth", authRouter);
+
+app.use(errorHandler);
 
 export { app };
