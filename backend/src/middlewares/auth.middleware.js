a03b2d01d9659abfdd36031d01e9d13412b0d381
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-12-06 01:42:47 +0530

    refactor: improve error handling and token validation in protectRoute middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,19 +2,26 @@ import jwt from 'jsonwebtoken';
 import ApiError from '../utils/ApiError.js';
 import User from '../../models/user.model.js';
 import { JWT_SECRET } from '../config/env.js';
+import pkg from "jsonwebtoken";
+import { th } from 'zod/locales';
+const { JsonWebTokenError } = pkg;
 
 export const protectRoute = async (req, res, next) => {
-    const accessToken = req.cookies?.accessToken || req.headers?.authorization?.split(" ")[1];
-    if (!accessToken) {
-        return next(new ApiError(401, "Unauthorized: No token provided"));
-    }
     try {
+        const accessToken = req.cookies?.accessToken || req.headers?.authorization?.split(" ")[1];
+        if (!accessToken) {
+            throw new ApiError(401, "Unauthorized: No token provided")
+        }
+
         const decodedToken = jwt.verify(accessToken, JWT_SECRET);
         const userId = decodedToken.userId;
         const user = await User.findById(userId).select("-password");
         req.user = user;
         next();
     } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            return next(new ApiError(401, "Unauthorized: Invalid token", error.message));
+        }
         next(error);
     }
 }
