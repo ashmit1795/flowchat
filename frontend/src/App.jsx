48068b469a6b6f64b4ac93a2ebff5f5e0fdb9b4a
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-12-06 01:53:09 +0530

    feat: implement user authentication checks for routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Onboarding from "./pages/Onboarding";
@@ -8,21 +8,36 @@ import Home from "./pages/Home";
 import Call from "./pages/Call";
 import Chat from "./pages/Chat";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./config/axios";
 
 const App = () => {
+	const {
+		data: authenticatedUserData,
+	} = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			const response = await axiosInstance.get("/auth/me");
+			return response.data;
+		},
+		retry: false, // Disable automatic retries
+	});
+
+	const authenticatedUser = authenticatedUserData?.data.user || null;
+
 	return (
 		<div className="h-screen" data-theme="night">
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/onboarding" element={<Onboarding />} />
-				<Route path="/notifications" element={<Notifications />} />
-				<Route path="/call" element={<Call />} />
-				<Route path="/chat" element={<Chat />} />
-      </Routes>
-      
-      <Toaster/>
+				<Route path="/" element={authenticatedUser ? <Home /> : <Navigate to="/login" />} />
+				<Route path="/login" element={!authenticatedUser ? <Login /> : <Navigate to="/" />} />
+				<Route path="/signup" element={!authenticatedUser ? <SignUp /> : <Navigate to="/" />} />
+				<Route path="/onboarding" element={authenticatedUser ? <Onboarding /> : <Navigate to="/login" />} />
+				<Route path="/notifications" element={authenticatedUser ? <Notifications /> : <Navigate to="/login" />} />
+				<Route path="/call" element={authenticatedUser ? <Call /> : <Navigate to="/login" />} />
+				<Route path="/chat" element={authenticatedUser ? <Chat /> : <Navigate to="/login" />} />
+			</Routes>
+
+			<Toaster />
 		</div>
 	);
 };
