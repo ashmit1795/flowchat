67d498ceadcf3a0edcb6740abcd0731c06270f5a
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-12-06 20:00:42 +0530

    feat: add language and avatar constants for localization and user representation

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,43 @@
+export const LANGUAGES = [
+	"English",
+	"Spanish",
+	"French",
+	"German",
+	"Mandarin",
+	"Japanese",
+	"Korean",
+	"Hindi",
+	"Russian",
+	"Portuguese",
+	"Arabic",
+	"Italian",
+	"Turkish",
+	"Dutch",
+];
+
+export const LANGUAGE_TO_FLAG = {
+	english: "gb",
+	spanish: "es",
+	french: "fr",
+	german: "de",
+	mandarin: "cn",
+	japanese: "jp",
+	korean: "kr",
+	hindi: "in",
+	russian: "ru",
+	portuguese: "pt",
+	arabic: "sa",
+	italian: "it",
+	turkish: "tr",
+	dutch: "nl",
+};
+
+export const FEMALE_AVATARS = [
+	71, 74, 57, 84, 64, 100, 83, 85, 66, 78, 65, 63, 81, 99, 90, 58, 95, 55, 68, 61, 89, 92, 54, 80, 79, 70, 97, 86, 88, 98, 73, 94, 87, 96, 72, 77,
+	56, 82, 69, 62, 93, 76, 59, 91, 53, 51, 67, 75, 60, 52,
+];
+
+export const MALE_AVATARS = [
+	13, 28, 38, 36, 39, 3, 23, 45, 42, 49, 4, 27, 6, 29, 12, 44, 17, 33, 40, 20, 35, 24, 22, 8, 18, 10, 41, 1, 47, 34, 2, 16, 32, 50, 30, 26, 15, 5,
+	31, 21, 37, 9, 48, 7, 25, 43, 19, 11, 14, 46,
+];
