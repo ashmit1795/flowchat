5e8648f39359d81bd94136cdad596e05248b1fe3
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-12-06 20:00:59 +0530

    fix: remove console log from signUp function

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,12 +1,16 @@
 import { axiosInstance } from "../config/axios";
 
 export const signUp = async (signupData) => {
-    console.log("Signing up with data:", signupData);
     const response = await axiosInstance.post("/auth/signup", signupData);
     return response.data;
 }
 
 export const getAuthUser = async () => {
     const response = await axiosInstance.get("/auth/me");
+    return response.data;
+}
+
+export const completeOnboarding = async (onboardData) => { 
+    const response = await axiosInstance.post("/auth/onboard", onboardData);
     return response.data;
 }
