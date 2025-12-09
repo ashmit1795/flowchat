0194a493c2537c3ccf205e2ed3cc2cb78a5b6aa6
ashmit1795 <139261819+ashmit1795@users.noreply.github.com>
2025-12-09 15:08:40 +0530

    feat: add CallButton and ChatLoader components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,8 @@ import Loading from "./Loading/Loading";
 import Layout from "./Layout/Layout";
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
+import ChatLoader from "./ChatLoader/ChatLoader";
+import ThemeSelector from "./ThemeSelector/ThemeSelector";
 
-export { Loading, Layout, Navbar, Sidebar };
+
+export { Loading, Layout, Navbar, Sidebar, ChatLoader, ThemeSelector };
